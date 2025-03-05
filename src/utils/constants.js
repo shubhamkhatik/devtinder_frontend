e710fe232f71ddc48dd229c3173fcfce8cf39e41
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-03-05 16:10:39 +0530

    deploy link issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:5454/api" : "/api";
-// export const BASE_URL = "https://devtinder-backend-s1q4.onrender.com/api";
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:5454/api" : "/api";
+export const BASE_URL = "https://devtinder-backend-s1q4.onrender.com";
