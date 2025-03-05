27474b82cb10320d6d0ac8b92b143c0c3eef15cb
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-03-05 16:17:23 +0530

    add localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 // export const BASE_URL =
-//   location.hostname === "localhost" ? "http://localhost:5454/api" : "/api";
-export const BASE_URL = "https://devtinder-backend-s1q4.onrender.com";
+//   location.hostname === "localhost"
+//     ? "http://localhost:5454/api"
+//     : "https://devtinder-backend-s1q4.onrender.com/api";
+export const BASE_URL = "https://devtinder-backend-s1q4.onrender.com/api";
