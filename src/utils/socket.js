7b85521ce10ede12de1f359c4f7ead4022a85dbf
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-07-21 15:52:45 +0530

    socket connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,21 @@
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
-export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/socket.io" });
-  }
+export const createSocketConnection = () => {  
+  return io(BASE_URL);
 };
+
+//https://socket.io/docs/v4/client-options/#path
+// if issue occur in aws nginx deployment 
+// ex. nginx config port 80 as api then write below code 
+
+// import io from "socket.io-client";
+// import { BASE_URL } from "./constants";
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     return io("/", { path: "/api/socket.io" });
+//   }
+// };
